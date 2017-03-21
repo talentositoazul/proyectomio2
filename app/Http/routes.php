6c9edef9e417e0a9d4e0440ef83307b8cfcabdcc
talentositoazul <cslozano0@misena.edu.co>
2017-03-21 17:14:45 -0500

    2017/03/21
    sistema de login y registro
    
    sigue documentacion

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,10 @@ Route::get('cinema', function(){
     return view('crear_pelicula');
 });
 
+Route::get('home', function(){
+    return view('home');
+});
+
 /*Route::post('crear_pelicula','PeliculasController@guardar');
 
 Route::get('listar','PeliculasController@index');
@@ -95,11 +99,21 @@ Route::post('actualizarreferencia/{Id_referencia}','referenciaController@actuali
 
 Route::get('eliminarreferencia/{Id_referencia}','referenciaController@eliminar');
 //Rutas referencia//
-
-
-
-
 //Proyecto//
+
+
+
+
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 
 
 
